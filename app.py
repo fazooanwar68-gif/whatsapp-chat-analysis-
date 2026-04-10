9fa2a8c3c828252e33ded4fa43b70cfe6a1e35dc
Fazila Anwar <fazooanwar68@gmail.com>
2026-04-10 10:40:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,7 @@ if uploaded_file is not None:
         df_wc = helper.create_wordcloud(selected_user,df)
         fig,ax = plt.subplots()
         ax.imshow(df_wc)
+        ax.axis("off")
         st.pyplot(fig)
 
         # most common words
